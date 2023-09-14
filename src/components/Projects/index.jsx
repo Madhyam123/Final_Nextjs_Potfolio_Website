787e093ebdefbd6b4406f59fe2f6d4b518b844d0
madhyam bharati <96839514+Madhyam123@users.noreply.github.com>
2023-09-15 00:04:10 +0530

    Update index.jsx

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -7,33 +7,38 @@ import gsap from 'gsap';
 import Image from 'next/image';
 import Rounded from '../../common/RoundedButton';
 import Link from 'next/link';
+
 const projects = [
   {
-    title: "C2 Montreal",
-    src: "c2montreal.png",
+    title: "ECOMMERCE WEBSITE",
+    src: "ecom.png",
     stack: "TECH STACK 1.REACT 2.TAILWINDCSS 3.REDUX 4.OUTH 5.STRIPE",
-    Feature:"1.React redux state management 2.Outh authentication 3.React chart admin dashboard 4.Stripe Payment Gateway",
+    Feature:"Features-1.React redux state management 2.Outh authentication 3.React chart admin dashboard 4.Stripe Payment Gateway",
     color: "#000000",
     href:"https://madhyamecommercesite.netlify.app/"
   
   },
   {
-    title: "Office Studio",
-    src: "officestudio.png",
+    title: "Machine Design",
+    src: "image 4.png",
     stack: "TECH STACK 1.HTML 2.CSS 3.JAVASCRIPT 4.GSAP 5.LOCOMOTIVE SCROLL",
-    Feature:"1.Animation 2.Best User Interface 3.Smooth Scroll 4.Onscroll effects",
+    Feature:"Features-1.Animation 2.Best User Interface 3.Smooth Scroll 4.Onscroll effects",
     color: "#8C8C8C",
     href:"https://madhyammachine.netlify.app/"
   },
   {
     title: "Locomotive",
     src: "locomotive.png",
+    stack: "TECH STACK 1.REACT 2.TAILWINDCSS 3.REDUX 4.OUTH 5.STRIPE",
+    Feature:"Features-1.React redux state management 2.Outh authentication 3.React chart admin dashboard 4.Stripe Payment Gateway",
     color: "#EFE8D3",
     href:"https://madhyamecommercesite.netlify.app/"
   },
   {
     title: "Silencio",
     src: "silencio.png",
+    stack: "TECH STACK 1.HTML 2.CSS 3.JAVASCRIPT 4.GSAP 5.LOCOMOTIVE SCROLL",
+    Feature:"Features-1.Animation 2.Best User Interface 3.Smooth Scroll 4.Onscroll effects",
     color: "#706D63",
     href:"https://madhyamecommercesite.netlify.app/"
   }
@@ -89,19 +94,15 @@ export default function Home() {
   <main onMouseMove={(e) => {moveItems(e.clientX, e.clientY)}} className={styles.projects}>
     <div className={styles.body}>
       {
-        projects.map( (project,  index) => {
-          return <Link className={styles.name} href={project.href}><Project  href={project.href}  index={index} stack={project.stack} 
-          Feature={project.Feature}
-          title={project.title} manageModal={manageModal} key={index}  /></Link>
-          
+        projects.map( (project, index) => {
+          return <Link className={styles.name} href={project.href}><Project  href={project.href}  index={index} title={project.title} stack={project.stack} 
+          Feature={project.Feature} manageModal={manageModal} key={index}  /></Link>
         })
       }
     </div>
-    <Link href='/project'>
     <Rounded>
       <p>More work</p>
     </Rounded>
-    </Link>
     <>
         <motion.div ref={modalContainer} variants={scaleAnimation} initial="initial" animate={active ? "enter" : "closed"} className={styles.modalContainer}>
             <div style={{top: index * -100 + "%"}} className={styles.modalSlider}>
