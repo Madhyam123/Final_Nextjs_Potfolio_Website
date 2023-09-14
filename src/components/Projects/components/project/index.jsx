71ec8d079109223bb17ffde3c6a0a826ae160421
madhyam bharati <96839514+Madhyam123@users.noreply.github.com>
2023-09-14 23:47:53 +0530

    Add files via upload

---
+++ src/components/Projects/components/project/index.jsx
@@ -0,0 +1,26 @@
+'use client';
+import React from 'react'
+import styles from './style.module.scss';
+
+export default function index({index, title,stack,Feature, manageModal}) {
+
+    return (
+        <div onMouseEnter={(e) => {manageModal(true, index, e.clientX, e.clientY)}} onMouseLeave={(e) => {manageModal(false, index, e.clientX, e.clientY)}} className={styles.project}>
+            <h2>{title}</h2>
+            <div>
+            <h3>{stack}</h3>
+            <h5>{Feature}</h5>
+            <button className={styles.btn}>View</button>
+            </div>
+            
+            {/* <h5>.MONGODB</h5>
+            <h5>.MONGODB</h5>
+            <h5>.MONGODB</h5>
+            <h5>.MONGODB</h5>
+            <h5>.MONGODB</h5>
+            <h5>.MONGODB</h5>
+            <h5>.MONGODB</h5>
+            <h5>.MONGODB</h5> */}
+        </div>
+    )
+}
